e331e9e1dfb9f66a29bb755e2365ebcc5d330a1c
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-04-26 19:32:46 +0200

    Add function to controller

--- models/allPlayerStatsBySeason.model.js
+++ models/allPlayerStatsBySeason.model.js
@@ -1,1 +1,33 @@
 import mongoose from 'mongoose';
+
+const allPlayerStatsBySeasonSchema = new mongoose.Schema(
+  {
+    player: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Player',
+      required: true,
+    },
+    season: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Season',
+    },
+    league: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'League',
+    },
+    playerStats: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'PlayerStats',
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const AllPlayerStatsBySeason = mongoose.model(
+  'AllPlayerStatsBySeason',
+  allPlayerStatsBySeasonSchema
+);
+
+export default AllPlayerStatsBySeason;
