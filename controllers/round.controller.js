ef7e9f1088ec35878ddff95b7478c1834b615424
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-05-29 10:24:01 +0200

    Add population of additional fields in getRoundByLeagueId

--- controllers/round.controller.js
+++ controllers/round.controller.js
@@ -1,6 +1,11 @@
 import League from '../models/league.model.js';
 import Match from '../models/match.model.js';
+import MatchStats from '../models/matchStats.model.js';
+import PlayerStats from '../models/playerStats.model.js';
+import Result from '../models/result.model.js';
 import Round from '../models/round.model.js';
+import Season from '../models/season.model.js';
+import TeamStats from '../models/teamStats.model.js';
 
 export const getRoundByLeagueId = async (req, res, next) => {
   try {
@@ -12,19 +17,59 @@ export const getRoundByLeagueId = async (req, res, next) => {
       .populate('homeTeam', 'name')
       .populate('awayTeam', 'name')
       .populate('league', 'name')
+      .populate('mainReferee', 'name surname')
+      .populate('firstAssistantReferee', 'name surname')
+      .populate('secondAssistantReferee', 'name surname')
       .select('round')
       .select('startDate'); // Only select the startDate field
 
     const matchesByRound = rounds.map((round) => {
       const matches = matchess
         .filter((match) => round.matches.includes(match._id.toString()))
-        .map(({ _id, homeTeam, awayTeam, startDate, ...rest }) => ({
-          matchId: _id,
-          homeTeam: homeTeam.name + ':' + homeTeam._id,
-          awayTeam: awayTeam.name + ':' + awayTeam._id,
-          startDate,
-          ...rest,
-        }));
+        .map(
+          ({
+            _id,
+            homeTeam,
+            awayTeam,
+            startDate,
+            mainReferee,
+            firstAssistantReferee,
+            secondAssistantReferee,
+            ...rest
+          }) => ({
+            matchId: _id,
+            homeTeam: homeTeam.name + ':' + homeTeam._id,
+            awayTeam: awayTeam.name + ':' + awayTeam._id,
+            mainReferee:
+              mainReferee !== null
+                ? mainReferee.name +
+                  ' ' +
+                  mainReferee.surname +
+                  ':' +
+                  mainReferee._id
+                : null,
+
+            firstAssistantReferee:
+              firstAssistantReferee !== null
+                ? firstAssistantReferee.name +
+                  ' ' +
+                  firstAssistantReferee.surname +
+                  ':' +
+                  firstAssistantReferee._id
+                : null,
+
+            secondAssistantReferee:
+              secondAssistantReferee !== null
+                ? secondAssistantReferee?.name +
+                  ' ' +
+                  secondAssistantReferee?.surname +
+                  ':' +
+                  secondAssistantReferee?._id
+                : null,
+            startDate,
+            ...rest,
+          })
+        );
 
       return { ...round._doc, matches };
     });
@@ -54,28 +99,51 @@ export const getRoundById = async (req, res, next) => {
   }
 };
 
-export const deleteRoundsAndMatches = async (req, res, next) => {
+export const deleteSchedule = async (req, res, next) => {
   try {
     const league = await League.findById(req.params.id);
     if (!league) {
       return res.status(404).json({ message: 'League not found' });
     }
 
+    const season = await Season.findById(league.season);
+
     const rounds = await Round.find({ league: req.params.id });
     if (!rounds) {
       return res.status(404).json({ message: 'Rounds not found' });
     }
 
-    const matches = await Match.find({ league: req.params.id });
+    const matches = await Match.find({
+      league: req.params.id,
+      season: season._id,
+    });
     if (!matches) {
       return res.status(404).json({ message: 'Matches not found' });
     }
+
+    const deletedResults = await Result.deleteMany({
+      match: { $in: matches.map((match) => match._id) },
+    });
+
+    const deletedMatchStats = await MatchStats.deleteMany({
+      match: { $in: matches.map((match) => match._id) },
+    });
+
+    const deletedTeamStats = await TeamStats.deleteMany({
+      match: { $in: matches.map((match) => match._id) },
+    });
 
     const deletedMatches = await Match.deleteMany({ league: req.params.id });
 
     const deletedRounds = await Round.deleteMany({ league: req.params.id });
 
-    res.status(201).json({ deletedMatches, deletedRounds });
+    res.status(201).json({
+      deletedMatches,
+      deletedRounds,
+      deletedMatchStats,
+      deletedResults,
+      deletedTeamStats,
+    });
   } catch (error) {
     next(error);
   }
