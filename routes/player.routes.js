ee79e2e5855063f3658202b4c2626f93aaf420c2
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-05-29 10:26:20 +0200

    Update player.routes.js to include new controller functions and file upload support

--- routes/player.routes.js
+++ routes/player.routes.js
@@ -6,16 +6,21 @@ import {
   getPlayersByWantedTeam,
   searchPlayers,
 } from '../controllers/player.controller.js';
-import { getAllPlayersGoalsBySeasonAndLeague } from '../controllers/allPlayerStatsBySeason.controller.js';
+import {
+  getAllPlayersStatsBySeasonAndLeague,
+  getAllPlayerStatsByPlayerId,
+} from '../controllers/allPlayerStatsBySeason.controller.js';
+import upload from '../utils/upload.js';
 
 const router = express.Router();
 
-router.post('/add', addPlayer);
+router.post('/add', upload.single('photo'), addPlayer);
 
 router.get('/get/:id', getPlayerByUserId);
 router.get('/:id', getPlayerById);
 router.get('/get-wanted-team/:id', getPlayersByWantedTeam);
-router.get('/top-stats/:leagueId', getAllPlayersGoalsBySeasonAndLeague);
+router.get('/top-stats/:leagueId', getAllPlayersStatsBySeasonAndLeague);
+router.get('/player-top-stats/:playerId', getAllPlayerStatsByPlayerId);
 router.get('/', searchPlayers);
 
 export default router;
