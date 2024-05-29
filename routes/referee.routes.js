9614cc2c080a0fd4d718dd1174367e6a65cd8df7
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-05-29 10:25:53 +0200

    Add new routes and controllers for referee functionality

--- routes/referee.routes.js
+++ routes/referee.routes.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import {
   addReferee,
+  assignRefereesToMatch,
+  getMatchDetailsDocx,
+  getMatchDetailsPDF,
+  getMatchDetailsXlsx,
+  getRefereeById,
   getRefereeByUserId,
 } from '../controllers/referee.controller.js';
 import {
@@ -10,17 +15,19 @@ import {
   getCompletedMatchesByLeagueId,
   getFilledMatchesByLeagueId,
   getMatchById,
+  getRefereeMatches,
   getSchedule,
   getSeasonByMatchId,
 } from '../controllers/match.controller.js';
 import {
-  deleteRoundsAndMatches,
+  deleteSchedule,
   getRoundById,
   getRoundByLeagueId,
 } from '../controllers/round.controller.js';
 import {
   addResult,
   getResultByMatchId,
+  getResultDetailsById,
 } from '../controllers/result.controller.js';
 import {
   addMatchStats,
@@ -28,10 +35,13 @@ import {
   removeMatchStatsByMatchId,
 } from '../controllers/matchStats.controller.js';
 
+import upload from '../utils/upload.js';
+
 const router = express.Router();
 
-router.post('/create', addReferee);
+router.post('/add', upload.single('photo'), addReferee);
 
+router.get('/:id', getRefereeById);
 router.get('/get/:id', getRefereeByUserId);
 router.get('/get-schedule/:id', getSchedule);
 router.get('/get-rounds/:id', getRoundByLeagueId);
@@ -41,13 +51,19 @@ router.get('/match-result/:id', getResultByMatchId);
 router.get('/match-stats/:id/:playerId', getMatchStatsByMatchId);
 router.get('/completed-matches/:id', getCompletedMatchesByLeagueId);
 router.get('/filled-matches/:id', getFilledMatchesByLeagueId);
+router.get('/referee-matches/:id', getRefereeMatches);
+router.get('/download-match-details-pdf/:id', getMatchDetailsPDF);
+router.get('/download-match-details-xlsx/:id', getMatchDetailsXlsx);
+router.get('/download-match-details-docx/:id', getMatchDetailsDocx);
 router.get('/season/:id', getSeasonByMatchId);
+router.get('/result-details/:resultId', getResultDetailsById);
 router.post('/generate-schedule/:id', generateMatchSchedule);
 router.post('/edit-match/:id', editMatch);
 router.post('/add-result', addResult);
+router.post('/assign-referees/:id', assignRefereesToMatch);
 router.post('/add-match-stats', addMatchStats);
 router.delete('/delete-matches/:id', deleteMatchesByLeagueId);
-router.delete('/delete-rounds/:id', deleteRoundsAndMatches);
+router.delete('/delete-schedule/:id', deleteSchedule);
 router.delete('/delete-stats/:matchId/:player', removeMatchStatsByMatchId);
 
 export default router;
