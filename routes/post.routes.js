1439b81b9204cd0e221aa67aee8297097d4f43b8
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-05-29 10:25:44 +0200

    Add post photo upload functionality and implement edit post, edit comment, add like, and remove like features

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,18 +1,27 @@
 import express from 'express';
 import {
   addCommentToPost,
+  addLikeToPost,
   addPost,
   deletePost,
+  editComment,
+  editPost,
   getPostById,
   getPosts,
+  removeLikeFromPost,
 } from '../controllers/post.controller.js';
+import upload from '../utils/upload.js';
 
 const router = express.Router();
 
-router.post('/add', addPost);
+router.post('/add', upload.single('postPhoto'), addPost);
 router.get('/all', getPosts);
 router.get('/get/:id', getPostById);
+router.post('/edit/:id', upload.single('postPhoto'), editPost);
+router.post('/comment/edit/:id', editComment);
+router.post('/like/:id', addLikeToPost);
 router.post('/comment/:id', addCommentToPost);
+router.post('/unlike/:id', removeLikeFromPost);
 router.delete('/delete', deletePost);
 
 export default router;
