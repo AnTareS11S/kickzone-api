8b368f5f6c7ad6498fb71454a229497442e08f59
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-06-05 17:35:11 +0200

    Refactor country controller functions

--- controllers/country.controller.js
+++ controllers/country.controller.js
@@ -26,20 +26,16 @@ export const getAllCountries = async (req, res, next) => {
   }
 };
 
-export const checkCountry = async (req, res, next) => {
+export const checkCountryName = async (req, res, next) => {
   try {
-    const { name, isEdit } = req.body;
-
-    if (isEdit) {
-      return res.status(200).json({ success: true });
-    }
+    const { name } = req.query;
 
     const existingCountry = await Country.findOne({ name });
-
     if (existingCountry) {
-      return res.status(200).json({ success: false });
+      res.status(200).json({ exists: true });
+    } else {
+      res.status(200).json({ exists: false });
     }
-
     res.status(200).json({ success: true });
   } catch (error) {
     next(error);
@@ -48,24 +44,23 @@ export const checkCountry = async (req, res, next) => {
 
 export const editCountry = async (req, res, next) => {
   try {
-    const country = await Country.findById(req.params.id);
+    const existedCountry = await Country.findById(req.params.id);
 
-    if (country) {
-      if (req.body.name && req.body.name !== country.name) {
-        const existingCountry = await Country.findOne({ name: req.body.name });
-        if (existingCountry) {
-          return res
-            .status(200)
-            .json({ success: false, message: 'Country name already exists!' });
-        }
-      }
-      const updatedCountry = await Country.findOneAndUpdate(
-        { _id: req.params.id },
-        { $set: req.body },
-        { new: true }
-      );
-      return res.status(200).json(updatedCountry);
+    if (!existedCountry) {
+      return res
+        .status(404)
+        .json({ success: false, message: 'Country not found!' });
     }
+
+    const updatedCountry = await Country.findByIdAndUpdate(
+      { _id: req.params.id },
+      { name: req.body.name },
+      {
+        new: true,
+      }
+    );
+
+    res.status(200).json(updatedCountry);
   } catch (error) {
     next(error);
   }
