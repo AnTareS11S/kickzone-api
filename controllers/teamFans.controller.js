d22aa95dda400fe577bd1c9c6783cc19124650d7
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-06-10 15:56:26 +0200

    Add team fans controller and related functions

---
+++ controllers/teamFans.controller.js
@@ -0,0 +1,76 @@
+import TeamFans from '../models/teamFans.model.js';
+import Team from '../models/team.model.js';
+
+export const addTeamFan = async (req, res, next) => {
+  try {
+    const existingFan = await TeamFans.findOne({
+      team: req.params.id,
+      user: req.body.userId,
+    });
+
+    if (existingFan) {
+      return res
+        .status(400)
+        .json({ message: 'User is already a fan of this team' });
+    }
+
+    const teamFan = await TeamFans.create({
+      team: req.params.id,
+      user: req.body.userId,
+    });
+
+    const team = await Team.findById(req.params.id);
+
+    team.fans.push(teamFan._id);
+
+    await team.save();
+
+    res.status(201).json(teamFan);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getTeamFansCount = async (req, res, next) => {
+  try {
+    const fansCount = await TeamFans.countDocuments({ team: req.params.id });
+
+    res.status(200).json(fansCount);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const checkIsTeamFan = async (req, res, next) => {
+  try {
+    const teamFan = await TeamFans.findOne({
+      team: req.params.id,
+      user: req.body.userId,
+    });
+
+    res.status(200).json({ isFan: !!teamFan });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const removeTeamFan = async (req, res, next) => {
+  try {
+    const exisitingFan = await TeamFans.findOne({
+      team: req.params.id,
+      user: req.body.userId,
+    });
+
+    const team = await Team.findById(req.params.id);
+
+    team.fans.pull(exisitingFan._id);
+
+    await team.save();
+
+    await TeamFans.findByIdAndDelete(exisitingFan._id);
+
+    res.status(200).json({ message: 'Team fan removed successfully' });
+  } catch (error) {
+    next(error);
+  }
+};
