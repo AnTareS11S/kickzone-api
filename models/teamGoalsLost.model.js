9f6d3ebb72e9f8ab2fd1585e6b4dcb6053088331
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-06-12 18:21:25 +0200

    Add teamGoalsLost model

---
+++ models/teamGoalsLost.model.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const teamGoalsLostSchema = new mongoose.Schema(
+  {
+    team: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Team',
+    },
+    season: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Season',
+    },
+    goals: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const TeamGoalsLost = mongoose.model('TeamGoalsLost', teamGoalsLostSchema);
+
+export default TeamGoalsLost;
