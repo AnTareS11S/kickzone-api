e35be3ae5872b6eaa9fcc6e57950ebfd57355877
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-06-13 16:31:03 +0200

    Add teamOutcomes controller for managing team statistics

---
+++ controllers/teamOutcomes.controller.js
@@ -0,0 +1,79 @@
+import TeamStats from '../models/teamStats.model.js';
+import TeamWins from '../models/teamWins.model.js';
+import TeamDraws from '../models/teamDraws.model.js';
+import TeamLosses from '../models/teamLosses.model.js';
+
+export const getTeamOutcomes = async (req, res, next) => {
+  try {
+    const teamStats = await TeamStats.findOne({
+      team: req.params.teamId,
+      season: req.query.season,
+    });
+
+    if (!teamStats) {
+      return res.status(404).json({ message: 'Team Stats not found' });
+    }
+
+    const teamWins = teamStats.wins;
+
+    const teamDraws = teamStats.draws;
+
+    const teamLosses = teamStats.losses;
+
+    const existingTeamWins = await TeamWins.findOne({
+      team: req.params.teamId,
+      season: req.query.season,
+    });
+
+    const existingTeamDraws = await TeamDraws.findOne({
+      team: req.params.teamId,
+      season: req.query.season,
+    });
+
+    const existingTeamLosses = await TeamLosses.findOne({
+      team: req.params.teamId,
+      season: req.query.season,
+    });
+
+    if (existingTeamWins) {
+      existingTeamWins.wins = teamWins;
+      await existingTeamWins.save();
+    } else {
+      await TeamWins.create({
+        team: req.params.teamId,
+        wins: teamWins,
+        season: req.query.season,
+      });
+    }
+
+    if (existingTeamDraws) {
+      existingTeamDraws.draws = teamDraws;
+      await existingTeamDraws.save();
+    } else {
+      await TeamDraws.create({
+        team: req.params.teamId,
+        draws: teamDraws,
+        season: req.query.season,
+      });
+    }
+
+    if (existingTeamLosses) {
+      existingTeamLosses.losses = teamLosses;
+      await existingTeamLosses.save();
+    } else {
+      await TeamLosses.create({
+        team: req.params.teamId,
+        losses: teamLosses,
+        season: req.query.season,
+      });
+    }
+
+    res.status(200).json({
+      wins: teamWins,
+      draws: teamDraws,
+      losses: teamLosses,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
