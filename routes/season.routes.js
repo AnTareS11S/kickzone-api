69370065859f2ef288f078f6ddaa6704652c9cc9
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-06-14 17:44:34 +0200

    Add getSeasonByLeagueId route to season.routes.js

--- routes/season.routes.js
+++ routes/season.routes.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import { checkSeasonName } from '../controllers/season.controller.js';
+import {
+  checkSeasonName,
+  getSeasonByLeagueId,
+} from '../controllers/season.controller.js';
 
 const router = express.Router();
 
 router.get('/check-season-name', checkSeasonName);
+router.get('/get-season/:leagueId', getSeasonByLeagueId);
 
 export default router;
