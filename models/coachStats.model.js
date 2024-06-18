406394c9134353b576912457c264bbeaae14c954
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-06-18 13:48:57 +0200

    Add coachStats model schema

---
+++ models/coachStats.model.js
@@ -0,0 +1,33 @@
+import monogoose from 'mongoose';
+
+const coachStatsSchema = new monogoose.Schema(
+  {
+    coach: {
+      type: monogoose.Schema.Types.ObjectId,
+      ref: 'Coach',
+      required: true,
+    },
+    team: {
+      type: monogoose.Schema.Types.ObjectId,
+      ref: 'Team',
+      required: true,
+    },
+    matches: {
+      type: Number,
+      required: true,
+    },
+    wins: {
+      type: Number,
+      required: true,
+    },
+    draws: {
+      type: Number,
+      required: true,
+    },
+    losses: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
