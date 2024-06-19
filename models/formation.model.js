41cb14739f6582f673b752be2e35ecd17ef8baa7
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-06-19 18:10:39 +0200

    Refactor formation model

--- models/formation.model.js
+++ models/formation.model.js
@@ -1,26 +1,27 @@
 import mongoose from 'mongoose';
 
-const formationSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    minlength: 2,
-    maxlength: 30,
+const formationSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 30,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 100,
+    },
   },
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 2,
-    maxlength: 100,
-  },
-  positions: {
-    type: [String],
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Formation = mongoose.model('Formation', formationSchema);
 
