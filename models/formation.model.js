c13b7164223df6a0472c50ac9cd8ed4b650e801b
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-06-19 18:04:12 +0200

    Add formation model definition

---
+++ models/formation.model.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const formationSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 2,
+    maxlength: 100,
+  },
+  positions: {
+    type: [String],
+    required: true,
+  },
+});
+
+const Formation = mongoose.model('Formation', formationSchema);
+
+export default Formation;
