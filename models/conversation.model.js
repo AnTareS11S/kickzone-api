8b72d7cbd904ab6913ac3eb8668630ba22dbfcbf
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-08-21 15:32:00 +0200

    Refactor conversation model to use 'members' instead of 'participants'

--- models/conversation.model.js
+++ models/conversation.model.js
@@ -2,37 +2,8 @@ import mongoose from 'mongoose';
 
 const conversationSchema = new mongoose.Schema(
   {
-    participants: [
-      {
-        user: {
-          type: mongoose.Schema.Types.ObjectId,
-          refPath: 'participants.role',
-        },
-        role: {
-          type: String,
-          required: true,
-        },
-        name: {
-          type: String,
-          required: true,
-        },
-        photo: {
-          type: String,
-          required: true,
-        },
-      },
-    ],
-    messages: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Message',
-      },
-    ],
-    lastMessage: {
-      type: String,
-    },
-    lastMessageTime: {
-      type: Date,
+    members: {
+      type: Array,
     },
   },
   { timestamps: true }
