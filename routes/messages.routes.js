8396beaba9f0c7c226b9f6cc343c36ab60fcec2a
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-08-22 16:21:22 +0200

    Add routes for sending and retrieving messages

--- routes/messages.routes.js
+++ routes/messages.routes.js
@@ -1,5 +1,12 @@
 import express from 'express';
+import {
+  getMessages,
+  sendMessage,
+} from '../controllers/messages.controller.js';
 
 const router = express.Router();
+
+router.post('/send-message', sendMessage);
+router.get('/:conversationId', getMessages);
 
 export default router;
