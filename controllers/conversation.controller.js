d933645161fcac9d7c695ef9779f45d8379b9a0a
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-08-24 16:04:08 +0200

    Refactor conversation controller and add new endpoint for conversations with two users

--- controllers/conversation.controller.js
+++ controllers/conversation.controller.js
@@ -1,6 +1,3 @@
-import Referee from '../models/referee.model.js';
-import Coach from '../models/coach.model.js';
-import Player from '../models/player.model.js';
 import Conversation from '../models/conversation.model.js';
 
 export const createConversation = async (req, res, next) => {
@@ -17,57 +14,6 @@ export const createConversation = async (req, res, next) => {
   }
 };
 
-export const getAllPeople = async (req, res, next) => {
-  try {
-    const search = req.query.term || '';
-    const searchTerms = search.split(' ').filter((term) => term.length > 0);
-
-    let searchQuery;
-    if (searchTerms.length > 1) {
-      searchQuery = {
-        $and: [
-          { name: { $regex: searchTerms[0], $options: 'i' } },
-          {
-            surname: { $regex: searchTerms.slice(1).join(' '), $options: 'i' },
-          },
-        ],
-      };
-    } else {
-      searchQuery = {
-        $or: [
-          { name: { $regex: search, $options: 'i' } },
-          { surname: { $regex: search, $options: 'i' } },
-        ],
-      };
-    }
-
-    const [players, coaches, referees] = await Promise.all([
-      Player.find(searchQuery),
-      Coach.find(searchQuery),
-      Referee.find(searchQuery),
-    ]);
-
-    const setImageUrl = (item) => {
-      if (item.photo) {
-        item.imageUrl = 'https://d3awt09vrts30h.cloudfront.net/' + item.photo;
-      } else {
-        item.imageUrl = null;
-      }
-      return item;
-    };
-
-    const modifyPlayers = players?.map((player) => setImageUrl(player));
-    const modifyCoaches = coaches?.map((coach) => setImageUrl(coach));
-    const modifyReferees = referees?.map((referee) => setImageUrl(referee));
-
-    const people = [...modifyPlayers, ...modifyCoaches, ...modifyReferees];
-
-    res.status(200).json(people);
-  } catch (error) {
-    next(error);
-  }
-};
-
 export const getConversations = async (req, res, next) => {
   try {
     const { userId } = req.params;
@@ -75,6 +21,22 @@ export const getConversations = async (req, res, next) => {
     const conversation = await Conversation.find({
       members: {
         $in: [userId],
+      },
+    });
+
+    res.status(200).json(conversation);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getConversationIncludesTwoUsers = async (req, res, next) => {
+  try {
+    const { firstUserId, secondUserId } = req.params;
+
+    const conversation = await Conversation.findOne({
+      members: {
+        $all: [firstUserId, secondUserId],
       },
     });
 
