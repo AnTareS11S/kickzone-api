b0bb7ae697c256a698eb44af909ce1d324646d57
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-09-10 17:26:24 +0200

    Add privacy controller with CRUD operations

---
+++ controllers/privacy.controller.js
@@ -0,0 +1,40 @@
+import Privacy from '../models/privacy.model.js';
+
+export const createPrivacy = async (req, res, next) => {
+  try {
+    const privacy = await Privacy.create(req.body);
+
+    res.status(200).json(privacy);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getPrivacies = async (req, res, next) => {
+  try {
+    const privacies = await Privacy.find();
+    res.status(200).json(privacies);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const editPrivacy = async (req, res, next) => {
+  try {
+    const privacy = await Privacy.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(privacy);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deletePrivacy = async (req, res, next) => {
+  try {
+    await Privacy.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: 'Privacy deleted successfully' });
+  } catch (error) {
+    next(error);
+  }
+};
