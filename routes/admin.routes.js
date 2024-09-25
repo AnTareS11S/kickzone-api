4f5edb2be65a577b23689294ad94d5ab69037cac
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-09-25 12:15:59 +0200

    Add match controller routes

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -17,7 +17,6 @@ import {
 } from '../controllers/country.controller.js';
 import {
   addStadium,
-  checkStadiumName,
   deleteStadium,
   editStadium,
   getStadiumById,
@@ -103,6 +102,10 @@ import {
   getAboutUs,
   getOneAboutUs,
 } from '../controllers/aboutUs.controller.js';
+import {
+  getRecentMatchResults,
+  getTodayMatches,
+} from '../controllers/match.controller.js';
 
 const customUpload = (req, res, next) => {
   if (
@@ -115,11 +118,11 @@ const customUpload = (req, res, next) => {
 };
 
 const router = express.Router();
-
+// User
 router.get('/user', getAllUsers);
 router.post('/users/:id/role', setRole);
 router.delete('/user/delete/:id', deleteUser);
-
+// Team
 router.get('/team', getAllTeams);
 router.get('/teams/no-league', getTeamsWithoutLeague);
 router.post('/team/add', customUpload, addTeam);
@@ -127,41 +130,41 @@ router.post('/team/edit/:id', customUpload, editTeam);
 router.post('/team-player/:id/add', addPlayerToTeam);
 router.delete('/team/delete/:id', deleteTeam);
 router.delete('/team-player/delete/:id', deletePlayerFromTeam);
-
+// Coach
 router.get('/coach', getAllCoaches);
 router.delete('/coach/delete/:id', deleteCoach);
-
+// League
 router.get('/league', getAllLeagues);
 router.post('/league/add', customUpload, addLeague);
 router.post('/league/edit/:id', customUpload, editLeague);
 router.post('/league/teams', getTeamsByIds);
 router.delete('/league/delete/:id', deleteLeague);
-
+// Country
 router.get('/country', getAllCountries);
 router.post('/country/add', customUpload, addCountry);
 router.post('/country/edit/:id', customUpload, editCountry);
 router.delete('/country/delete/:id', deleteCountry);
-
+// Stadium
 router.get('/stadium', getStadiums);
 router.get('/stadium/:stadiumId', getStadiumById);
 router.post('/stadium/add', customUpload, addStadium);
 router.post('/stadium/edit/:id', customUpload, editStadium);
 router.delete('/stadium/delete/:id', deleteStadium);
-
+// Position
 router.get('/position', getPositions);
 router.post('/position/add', customUpload, addPosition);
 router.post('/position/edit/:id', customUpload, editPosition);
 router.delete('/position/delete/:id', deletePosition);
-
+// Formation
 router.get('/formation', getFormations);
 router.post('/formation/add', customUpload, addFormation);
 router.post('/formation/edit/:id', customUpload, editFormation);
 router.delete('/formation/delete/:id', deleteFormation);
-
+// Player
 router.get('/player', getPlayers);
 router.get('/team-player/:id', getPlayersByCurrentTeam);
 router.delete('/player/delete/:id', deletePlayer);
-
+// Training
 router.get('/training', getAllCoachTrainings);
 router.get('/training/:id', getAllCoachTrainings);
 router.get('/participants/:id', getParticipants);
@@ -172,43 +175,46 @@ router.post('/training/edit/:id', customUpload, editTraining);
 router.post('/training/attendace/:id', addOrRemoveAttendace);
 router.delete('/training/delete/:id', deleteTraining);
 router.delete('/participants/delete/:id', removePlayerFromTraining);
-
+// Training Type
 router.get('/training-type', getAllCoachTrainingTypes);
 router.get('/training-type/:id', getAllCoachTrainingTypes);
 router.post('/training-type/:id/add', customUpload, addTrainingType);
 router.post('/training-type/edit/:id', customUpload, editTrainingType);
 router.delete('/training-type/delete/:id', deleteTrainingType);
-
+// Season
 router.get('/season', getSeasons);
 router.post('/season/add', customUpload, addSeason);
 router.post('/season/edit/:id', customUpload, editSeason);
 router.delete('/season/delete/:id', deleteSeason);
-
+// Referee
 router.get('/referee', getAllReferees);
 router.delete('/referee/delete/:id', deleteReferee);
-
+// Sponsor
 router.get('/sponsor', getAllSponsors);
 router.post('/sponsor/add', customUpload, addSponsor);
 router.post('/sponsor/edit/:id', customUpload, editSponsor);
 router.delete('/sponsor/delete/:id', deleteSponsor);
-
+// Notification
 router.post('/notification/:recipientId', sendNotification);
 router.get('/get-notification/:recipientId', sendNotification);
-
+// Terms
 router.get('/terms', getTerms);
 router.post('/terms/add', customUpload, createTerm);
 router.post('/terms/edit/:id', customUpload, editTerm);
 router.delete('/terms/delete/:id', deleteTerm);
-
+// Privacy
 router.get('/privacy', getPrivacies);
 router.post('/privacy/add', customUpload, createPrivacy);
 router.post('/privacy/edit/:id', customUpload, editPrivacy);
 router.delete('/privacy/delete/:id', deletePrivacy);
-
+// About Us
 router.get('/about', getAboutUs);
 router.get('/aboutOne', getOneAboutUs);
 router.post('/about/add', upload.single('logo'), createAboutUs);
 router.post('/about/edit/:id', upload.single('logo'), editAboutUs);
 router.delete('/about/delete/:id', deleteAboutUs);
+// Explore
+router.get('/today-matches', getTodayMatches);
+router.get('/recent-results', getRecentMatchResults);
 
 export default router;
