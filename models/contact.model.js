53116839fba11e70ed8e187755133b368d6679b9
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-10-05 20:37:56 +0200

    Refactor contact model to include phone and address fields

--- models/contact.model.js
+++ models/contact.model.js
@@ -2,10 +2,11 @@ import mongoose from 'mongoose';
 
 const contactSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true },
-    message: { type: String, required: true },
+    name: { type: String, required: true, trim: true },
+    lastName: { type: String, required: true, trim: true },
+    email: { type: String, required: true, trim: true },
+    phone: { type: String, required: true, trim: true },
+    address: { type: String, required: true, trim: true },
   },
   { timestamps: true }
 );
