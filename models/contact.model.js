4d501456444363143608d7c9a37881eeba6d3610
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-10-08 16:14:16 +0200

    Refactor contact model to include active field

--- models/contact.model.js
+++ models/contact.model.js
@@ -7,6 +7,7 @@ const contactSchema = new mongoose.Schema(
     email: { type: String, required: true, trim: true },
     phone: { type: String, required: true, trim: true },
     address: { type: String, required: true, trim: true },
+    active: { type: Boolean, default: true },
   },
   { timestamps: true }
 );
