69149d413962f55b008d35228b8dd15b4ed79b7e
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-10-10 14:49:53 +0200

    Refactor admin.model.js to include admin schema with user, name, surname, photo, imageUrl, bio, nationality, city, and birthDate fields

---
+++ models/admin.model.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+const adminSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 30,
+    },
+    surname: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 30,
+    },
+    photo: {
+      type: String,
+    },
+    imageUrl: {
+      type: String,
+      default:
+        'https://d3awt09vrts30h.cloudfront.net/blank-profile-picture.webp',
+    },
+    bio: { type: String, default: '' },
+    nationality: { type: mongoose.Schema.Types.ObjectId, ref: 'Country' },
+    city: { type: String, required: true },
+    birthDate: { type: Date, required: true },
+  },
+  { timestamps: true }
+);
+
+const Admin = mongoose.model('Admin', adminSchema);
+
+export default Admin;
