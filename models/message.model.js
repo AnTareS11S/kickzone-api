7ee3769b2f2220a6364d19639917ff63eb57903d
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-10-17 15:09:45 +0200

    Add receiver field and isRead flag to message model

--- models/message.model.js
+++ models/message.model.js
@@ -12,9 +12,18 @@ const messageSchema = new mongoose.Schema(
       ref: 'User',
       required: true,
     },
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
     text: {
       type: String,
       required: true,
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
     },
   },
   { timestamps: true }
