a5a1e3e5e1dc68f1ad7f499cd29d1ae02c5c71e9
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-10-18 19:16:29 +0200

    Refactor conversation.routes.js to add routes for retrieving unread conversations and marking conversations as read

--- routes/conversation.routes.js
+++ routes/conversation.routes.js
@@ -4,6 +4,8 @@ import {
   deleteConversation,
   getConversationIncludesTwoUsers,
   getConversations,
+  getUnreadConversations,
+  markConversationAsRead,
 } from '../controllers/conversation.controller.js';
 
 const router = express.Router();
@@ -11,6 +13,8 @@ const router = express.Router();
 router.post('/', createConversation);
 router.get('/:userId', getConversations);
 router.get('/find/:firstUserId/:secondUserId', getConversationIncludesTwoUsers);
+router.get('/unread/:userId', getUnreadConversations);
+router.post('/mark-as-read', markConversationAsRead);
 router.delete('/delete/:conversationId', deleteConversation);
 
 export default router;
