82cd8f8504f1cebf8a7bb1d166f03c4a637a81b6
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-10-18 19:14:58 +0200

    Refactor conversation.controller.js to include models for Admin, Coach, Player, and Referee

--- controllers/conversation.controller.js
+++ controllers/conversation.controller.js
@@ -1,5 +1,9 @@
+import Admin from '../models/admin.model.js';
+import Coach from '../models/coach.model.js';
 import Conversation from '../models/conversation.model.js';
 import Message from '../models/message.model.js';
+import Player from '../models/player.model.js';
+import Referee from '../models/referee.model.js';
 
 export const createConversation = async (req, res, next) => {
   try {
@@ -42,6 +46,56 @@ export const getConversationIncludesTwoUsers = async (req, res, next) => {
     });
 
     res.status(200).json(conversation);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUnreadConversations = async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+
+    const conversations = await Conversation.find({
+      members: {
+        $in: [userId],
+      },
+    });
+
+    let unreadConversationsCount = 0;
+
+    await Promise.all(
+      conversations.map(async (conversation) => {
+        const unreadMessages = await Message.find({
+          conversation: conversation._id,
+          isRead: false,
+        });
+
+        if (unreadMessages.length > 0) {
+          unreadConversationsCount += 1;
+        }
+      })
+    );
+
+    res.status(200).json(unreadConversationsCount);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const markConversationAsRead = async (req, res, next) => {
+  try {
+    const { conversationId, userId } = req.body;
+
+    await Message.updateMany(
+      {
+        conversation: conversationId,
+        receiver: userId,
+        isRead: false,
+      },
+      { $set: { isRead: true } }
+    );
+
+    res.status(200).json({ success: true });
   } catch (error) {
     next(error);
   }
