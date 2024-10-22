bf4017bb5763eb1148d54ed88f2c10e04858e192
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-10-22 11:00:03 +0200

    Refactor notifications.routes.js to update routes for retrieving unread notifications count and notification details

--- routes/notifications.routes.js
+++ routes/notifications.routes.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import {
-  getNotifications,
+  getNotificationsDetails,
+  getUnreadNotificationsCount,
   markNotificationAsRead,
 } from '../controllers/notifications.controller.js';
 
 const router = express.Router();
 
-router.get('/unread/:receiverId', getNotifications);
+router.get('/unread-count/:receiverId', getUnreadNotificationsCount);
+router.get('/details/:receiverId', getNotificationsDetails);
 router.post('/markAsRead/:receiverId', markNotificationAsRead);
 
 export default router;
