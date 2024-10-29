88b453be8d61d61df158bd77ed67684a8ade8c7d
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-10-29 17:48:17 +0100

    Add markAllNotificationsAsRead route to notifications.routes.js

--- routes/notifications.routes.js
+++ routes/notifications.routes.js
@@ -3,6 +3,7 @@ import {
   deleteNotification,
   getNotificationsDetails,
   getUnreadNotificationsCount,
+  markAllNotificationsAsRead,
   markNotificationAsRead,
 } from '../controllers/notifications.controller.js';
 
@@ -11,6 +12,7 @@ const router = express.Router();
 router.get('/unread-count/:receiverId', getUnreadNotificationsCount);
 router.get('/details/:receiverId', getNotificationsDetails);
 router.post('/mark-as-read/:receiverId', markNotificationAsRead);
+router.post('/mark-all-as-read/:receiverId', markAllNotificationsAsRead);
 router.delete('/delete/:receiverId', deleteNotification);
 
 export default router;
