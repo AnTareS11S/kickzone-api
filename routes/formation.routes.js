e0cf174ce20437e36cce9789b4154013cf8a7ae6
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-12-20 19:17:07 +0100

    Remove formation model, controller, and routes

--- routes/formation.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { checkFormationName } from '../controllers/formation.controller.js';
-
-const router = express.Router();
-
-router.get('/check-formation-name', checkFormationName);
-
-export default router;
