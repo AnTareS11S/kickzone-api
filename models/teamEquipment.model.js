019d21495f71dc81bddb12bfcbdd4c4d9cc2b4bf
Krzysztof Zieliński <kzielinski50@gmail.com>
2024-12-28 18:15:37 +0100

    Add teamEquipment model to manage equipment details for teams

---
+++ models/teamEquipment.model.js
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+
+const teamEquipmentSchema = new mongoose.Schema(
+  {
+    team: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Team',
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 30,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      trim: true,
+      min: 0,
+      max: 999,
+    },
+    condition: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 30,
+    },
+    status: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 30,
+    },
+    lastChecked: {
+      type: Date,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const TeamEquipment = mongoose.model('TeamEquipment', teamEquipmentSchema);
+
+export default TeamEquipment;
