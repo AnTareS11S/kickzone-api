feb464d628e4f334da408eaa29cb1ec0c54f82b0
Krzysztof Zieliński <kzielinski50@gmail.com>
2025-01-07 21:23:23 +0100

    Add team forum category controller with CRUD operations

---
+++ controllers/teamForumCategory.controller.js
@@ -0,0 +1,52 @@
+import ThreadCategory from '../models/threadCategory.model.js';
+
+export const addTeamForumCategory = async (req, res, next) => {
+  try {
+    const teamForumCategory = new ThreadCategory(req.body);
+    await teamForumCategory.save();
+    res.status(201).json(teamForumCategory);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getTeamForumCategories = async (req, res, next) => {
+  try {
+    const teamForumCategories = await ThreadCategory.find();
+
+    if (!teamForumCategories) {
+      return res
+        .status(404)
+        .json({ success: false, message: 'No team forum categories found!' });
+    }
+
+    res.status(200).json(teamForumCategories);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const editTeamForumCategory = async (req, res, next) => {
+  try {
+    const teamForumCategory = await ThreadCategory.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+      }
+    );
+
+    res.status(200).json(teamForumCategory);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteTeamForumCategory = async (req, res, next) => {
+  try {
+    await ThreadCategory.findByIdAndDelete(req.params.id);
+    res.status(200).json({ success: true });
+  } catch (error) {
+    next(error);
+  }
+};
