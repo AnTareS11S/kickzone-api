f15520803936ad22e7a83455e39ec4bde1d521eb
Krzysztof Zieliński <kzielinski50@gmail.com>
2025-01-19 16:13:04 +0100

    Add thread and comment management routes for editing, liking, and deletion

--- routes/forum.routes.js
+++ routes/forum.routes.js
@@ -1,12 +1,28 @@
 import express from 'express';
 import {
+  addCommentToThread,
   addNewThread,
+  deleteComment,
+  deleteThread,
+  editComment,
+  editThread,
+  getThreadById,
   getThreads,
+  handleLikeComment,
+  handleLikeThread,
 } from '../controllers/teamForum.controller.js';
 
 const router = express.Router();
 
 router.post('/new-thread', addNewThread);
+router.post('/thread/edit/:threadId', editThread);
+router.post('/thread/:threadId/like', handleLikeThread);
+router.post('/thread/:threadId/comment', addCommentToThread);
+router.post('/comment/edit/:commentId', editComment);
+router.post('/comment/:commentId/like', handleLikeComment);
 router.get('/threads', getThreads);
+router.get('/thread/:threadId', getThreadById);
+router.delete('/thread/delete/:threadId', deleteThread);
+router.delete('/comment/delete/:commentId', deleteComment);
 
 export default router;
