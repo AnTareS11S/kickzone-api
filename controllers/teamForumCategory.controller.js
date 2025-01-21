77d5360534916911b3c8fd0cab48f2d47b661051
Krzysztof Zieliński <kzielinski50@gmail.com>
2025-01-21 13:28:48 +0100

    Enhance team forum category retrieval to include team-specific counts

--- controllers/teamForumCategory.controller.js
+++ controllers/teamForumCategory.controller.js
@@ -1,3 +1,5 @@
+import mongoose from 'mongoose';
+import Thread from '../models/thread.model.js';
 import ThreadCategory from '../models/threadCategory.model.js';
 
 export const addTeamForumCategory = async (req, res, next) => {
@@ -12,7 +14,35 @@ export const addTeamForumCategory = async (req, res, next) => {
 
 export const getTeamForumCategories = async (req, res, next) => {
   try {
-    const teamForumCategories = await ThreadCategory.find().sort({ order: 1 });
+    const AuthorModel = mongoose.model(req.params.model);
+    const author = await AuthorModel.findOne({ user: req.params.userId });
+    const teamId = author?.currentTeam;
+
+    const categories = await ThreadCategory.find().sort({ order: 1 });
+
+    const categoriesWithTeamCounts = categories.map((category) => {
+      const teamCount = category.teamCounts.find(
+        (tc) => tc.teamId.toString() === teamId.toString()
+      );
+
+      return {
+        ...category.toObject(),
+        count: teamCount?.count || 0,
+        teamCounts: undefined,
+      };
+    });
+
+    res.status(200).json(categoriesWithTeamCounts);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getTeamForumCategoriesToSelect = async (req, res, next) => {
+  try {
+    const teamForumCategories = await ThreadCategory.find({}).sort({
+      order: 1,
+    });
 
     if (!teamForumCategories) {
       return res
