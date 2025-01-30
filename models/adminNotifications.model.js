f37fdefe7ac05518234e0c87da1bce8be48e1f9c
Krzysztof Zieliński <kzielinski50@gmail.com>
2025-01-30 17:25:04 +0100

    Add admin notification model with schema definition

---
+++ models/adminNotifications.model.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const adminNotificationSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+    },
+    content: {
+      type: String,
+      trim: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: ['newUser', 'newTeam', 'report'],
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const AdminNotification = mongoose.model(
+  'AdminNotification',
+  adminNotificationSchema
+);
+
+export default AdminNotification;
